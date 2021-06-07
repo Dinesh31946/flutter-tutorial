98a0545238820c9bead9cc26eb46232bb2380609
Dinesh31946 <gosavidinesh54@gmail.com>
2021-06-07 16:20:14 +0530

    Used Api and get catalog data from API

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:first_app/models/catalog.dart';
 import 'package:first_app/widgets/home_widgets/add_to_cart.dart';
-import 'package:first_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
